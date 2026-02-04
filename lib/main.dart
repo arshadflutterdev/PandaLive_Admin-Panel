8d0a43cd41f7c3a293a5ab382b695dc3013a38e8
arshadflutterdev <arshadcodess@gmail.com>
2026-02-04 15:37:32 +0500

    changes

--- lib/main.dart
+++ lib/main.dart
@@ -1,6 +1,6 @@
 import 'package:firebase_core/firebase_core.dart';
 import 'package:flutter/material.dart';
-import 'package:panda_adminpanel/Authentication/login_screen.dart';
+import 'package:panda_adminpanel/AdminPanel/Authentication/login_screen.dart';
 import 'package:panda_adminpanel/firebase_options.dart';
 
 void main() async {
