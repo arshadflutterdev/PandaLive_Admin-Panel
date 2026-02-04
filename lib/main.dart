80d8a85303c4358c9abb11af67d9c6884966ad28
arshadflutterdev <arshadcodess@gmail.com>
2026-02-04 15:25:02 +0500

    changes

--- lib/main.dart
+++ lib/main.dart
@@ -1,11 +1,15 @@
+import 'package:firebase_core/firebase_core.dart';
 import 'package:flutter/material.dart';
+import 'package:panda_adminpanel/firebase_options.dart';
 
-void main() {
+void main() async {
+  WidgetsFlutterBinding.ensureInitialized();
+  await Firebase.initializeApp(options: DefaultFirebaseOptions.currentPlatform);
   runApp(const MyApp());
 }
 
 class MyApp extends StatelessWidget {
-  const MyApp({super.key});jjjjjjj
+  const MyApp({super.key});
 
   // This widget is the root of your application.
   @override
