d0a2e4cefb2086f9b7ae91dcd36245d6981c0bcb
arshadflutterdev <arshadcodess@gmail.com>
2026-02-05 13:40:50 +0500

    changes

---
+++ lib/AdminPanel/Panel_UI/AppUsersScreens/app_users_screen.dart
@@ -0,0 +1,15 @@
+import 'package:flutter/material.dart';
+
+class AppUsersScreen extends StatefulWidget {
+  const AppUsersScreen({super.key});
+
+  @override
+  State<AppUsersScreen> createState() => _AppUsersScreenState();
+}
+
+class _AppUsersScreenState extends State<AppUsersScreen> {
+  @override
+  Widget build(BuildContext context) {
+    return const Placeholder();
+  }
+}
--- lib/AdminPanel/Panel_UI/HomeScreen/home_screen.dart
+++ lib/AdminPanel/Panel_UI/HomeScreen/home_screen.dart
@@ -2,6 +2,11 @@ import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
 import 'package:gap/gap.dart';
 import 'package:get/state_manager.dart';
+import 'package:panda_adminpanel/AdminPanel/Panel_UI/AboutMeScreens/about_me.dart';
+import 'package:panda_adminpanel/AdminPanel/Panel_UI/AppUsersScreens/app_users_screen.dart';
+import 'package:panda_adminpanel/AdminPanel/Panel_UI/GraphScreen/graph_screen.dart';
+import 'package:panda_adminpanel/AdminPanel/Panel_UI/SettingsScreen/settings_screen.dart';
+import 'package:panda_adminpanel/AdminPanel/Panel_UI/Wallet/wallet_screen.dart';
 import 'package:panda_adminpanel/AdminPanel/Utils/Constants/app_colours.dart';
 
 class HomeScreen extends StatefulWidget {
@@ -12,7 +17,6 @@ class HomeScreen extends StatefulWidget {
 }
 
 class _HomeScreenState extends State<HomeScreen> {
-  RxInt seletedTab = 0.obs;
   List<String> tabNames = [
     "Home",
     "AppUsers",
@@ -29,6 +33,15 @@ class _HomeScreenState extends State<HomeScreen> {
     Icon(Icons.settings),
     Icon(CupertinoIcons.profile_circled),
   ];
+  List<Widget> adminScreens = [
+    HomeScreen(),
+    GraphScreen(),
+    SettingsScreen(),
+    AppUsersScreen(),
+    WalletScreen(),
+    AboutMe(),
+  ];
+  RxInt selectedIndex = 0.obs;
   @override
   Widget build(BuildContext context) {
     final height = MediaQuery.of(context).size.height;
@@ -62,35 +75,41 @@ class _HomeScreenState extends State<HomeScreen> {
                   child: ListView.builder(
                     itemCount: 5,
                     itemBuilder: (context, index) {
-                      return Padding(
-                        padding: const EdgeInsets.symmetric(
-                          horizontal: 10,
-                          vertical: 3,
-                        ),
-                        child: Container(
-                          width: double.infinity,
-                          decoration: BoxDecoration(
-                            color: Colors.white,
-                            borderRadius: BorderRadius.circular(10),
+                      return Obx(
+                        () => Padding(
+                          padding: const EdgeInsets.symmetric(
+                            horizontal: 10,
+                            vertical: 3,
                           ),
-                          child: Padding(
-                            padding: const EdgeInsets.symmetric(vertical: 8),
-                            child: Wrap(
-                              spacing: 10,
-                              alignment: WrapAlignment.center,
-                              runSpacing: 10,
-                              children: [
-                                Text(
-                                  tabNames[index].toString(),
-
-                                  style: TextStyle(
-                                    fontSize: 18,
-                                    fontWeight: FontWeight.bold,
-                                  ),
+                          child: GestureDetector(
+                            child: Container(
+                              width: double.infinity,
+                              decoration: BoxDecoration(
+                                color: Colors.white,
+                                borderRadius: BorderRadius.circular(10),
+                              ),
+                              child: Padding(
+                                padding: const EdgeInsets.symmetric(
+                                  vertical: 8,
                                 ),
+                                child: Wrap(
+                                  spacing: 10,
+                                  alignment: WrapAlignment.center,
+                                  runSpacing: 10,
+                                  children: [
+                                    Text(
+                                      tabNames[index].toString(),
 
-                                tabIcons[index],
-                              ],
+                                      style: TextStyle(
+                                        fontSize: 18,
+                                        fontWeight: FontWeight.bold,
+                                      ),
+                                    ),
+
+                                    tabIcons[index],
+                                  ],
+                                ),
+                              ),
                             ),
                           ),
                         ),
