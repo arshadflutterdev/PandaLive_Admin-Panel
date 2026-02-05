162f25f3ee32b773e74f61ffdf92d172ccf0f02d
arshadflutterdev <arshadcodess@gmail.com>
2026-02-05 13:01:58 +0500

    changes

--- lib/AdminPanel/Panel_UI/HomeScreen/home_screen.dart
+++ lib/AdminPanel/Panel_UI/HomeScreen/home_screen.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:gap/gap.dart';
 import 'package:panda_adminpanel/AdminPanel/Utils/Constants/app_colours.dart';
 
 class HomeScreen extends StatefulWidget {
@@ -19,21 +20,236 @@ class _HomeScreenState extends State<HomeScreen> {
       body: Row(
         children: [
           Container(
-            width: width * 0.15,
-            decoration: BoxDecoration(color: Colors.blue),
+            width: width * 0.16,
+            decoration: BoxDecoration(color: Color(0xff0D1A63)),
             child: Column(
+              mainAxisAlignment: MainAxisAlignment.start,
               children: [
-                SizedBox(
-                  height: 60,
-                  width: double.infinity,
-                  child: DrawerHeader(
-                    child: Text(
-                      "Panda Admin",
-                      style: TextStyle(fontSize: 16, color: Colors.white),
-                    ),
+                Gap(10),
+                Center(
+                  child: Text(
+                    "Panda Admin",
+                    style: TextStyle(fontSize: 22, color: Colors.white),
                   ),
                 ),
+                Center(
+                  child: Text(
+                    "zahrani@gmail.com",
+                    style: TextStyle(fontSize: 12, color: Colors.white),
+                  ),
+                ),
+                Gap(20),
+                Column(
+                  children: [
+                    Padding(
+                      padding: const EdgeInsets.symmetric(
+                        horizontal: 10,
+                        vertical: 3,
+                      ),
+                      child: Container(
+                        width: double.infinity,
+                        decoration: BoxDecoration(
+                          color: Colors.white,
+                          borderRadius: BorderRadius.circular(10),
+                        ),
+                        child: Padding(
+                          padding: const EdgeInsets.symmetric(vertical: 8),
+                          child: Wrap(
+                            spacing: 10,
+                            alignment: WrapAlignment.center,
+                            runSpacing: 10,
+                            children: [
+                              Text(
+                                "Home",
+
+                                style: TextStyle(
+                                  fontSize: 20,
+                                  fontWeight: FontWeight.bold,
+                                ),
+                              ),
+
+                              Icon(Icons.home),
+                            ],
+                          ),
+                        ),
+                      ),
+                    ),
+                    Padding(
+                      padding: const EdgeInsets.symmetric(
+                        horizontal: 10,
+                        vertical: 3,
+                      ),
+                      child: Container(
+                        width: double.infinity,
+                        decoration: BoxDecoration(
+                          color: Colors.white,
+                          borderRadius: BorderRadius.circular(10),
+                        ),
+                        child: Padding(
+                          padding: const EdgeInsets.symmetric(vertical: 8),
+                          child: Wrap(
+                            spacing: 10,
+                            alignment: WrapAlignment.center,
+                            runSpacing: 10,
+                            children: [
+                              Text(
+                                "Home",
+
+                                style: TextStyle(
+                                  fontSize: 20,
+                                  fontWeight: FontWeight.bold,
+                                ),
+                              ),
+
+                              Icon(Icons.home),
+                            ],
+                          ),
+                        ),
+                      ),
+                    ),
+
+                    Padding(
+                      padding: const EdgeInsets.symmetric(
+                        horizontal: 10,
+                        vertical: 3,
+                      ),
+                      child: Container(
+                        width: double.infinity,
+                        decoration: BoxDecoration(
+                          color: Colors.white,
+                          borderRadius: BorderRadius.circular(10),
+                        ),
+                        child: Padding(
+                          padding: const EdgeInsets.symmetric(vertical: 8),
+                          child: Wrap(
+                            spacing: 10,
+                            alignment: WrapAlignment.center,
+                            runSpacing: 10,
+                            children: [
+                              Text(
+                                "Home",
+
+                                style: TextStyle(
+                                  fontSize: 20,
+                                  fontWeight: FontWeight.bold,
+                                ),
+                              ),
+
+                              Icon(Icons.home),
+                            ],
+                          ),
+                        ),
+                      ),
+                    ),
+                    Padding(
+                      padding: const EdgeInsets.symmetric(
+                        horizontal: 10,
+                        vertical: 3,
+                      ),
+                      child: Container(
+                        width: double.infinity,
+                        decoration: BoxDecoration(
+                          color: Colors.white,
+                          borderRadius: BorderRadius.circular(10),
+                        ),
+                        child: Padding(
+                          padding: const EdgeInsets.symmetric(vertical: 8),
+                          child: Wrap(
+                            spacing: 10,
+                            alignment: WrapAlignment.center,
+                            runSpacing: 10,
+                            children: [
+                              Text(
+                                "Home",
+
+                                style: TextStyle(
+                                  fontSize: 20,
+                                  fontWeight: FontWeight.bold,
+                                ),
+                              ),
+
+                              Icon(Icons.home),
+                            ],
+                          ),
+                        ),
+                      ),
+                    ),
+
+                    Padding(
+                      padding: const EdgeInsets.symmetric(
+                        horizontal: 10,
+                        vertical: 3,
+                      ),
+                      child: Container(
+                        width: double.infinity,
+                        decoration: BoxDecoration(
+                          color: Colors.white,
+                          borderRadius: BorderRadius.circular(10),
+                        ),
+                        child: Padding(
+                          padding: const EdgeInsets.symmetric(vertical: 8),
+                          child: Wrap(
+                            spacing: 10,
+                            alignment: WrapAlignment.center,
+                            runSpacing: 10,
+                            children: [
+                              Text(
+                                "Home",
+
+                                style: TextStyle(
+                                  fontSize: 20,
+                                  fontWeight: FontWeight.bold,
+                                ),
+                              ),
+
+                              Icon(Icons.home),
+                            ],
+                          ),
+                        ),
+                      ),
+                    ),
+                    Padding(
+                      padding: const EdgeInsets.symmetric(
+                        horizontal: 10,
+                        vertical: 3,
+                      ),
+                      child: Container(
+                        width: double.infinity,
+                        decoration: BoxDecoration(
+                          color: Colors.white,
+                          borderRadius: BorderRadius.circular(10),
+                        ),
+                        child: Padding(
+                          padding: const EdgeInsets.symmetric(vertical: 8),
+                          child: Wrap(
+                            spacing: 10,
+                            alignment: WrapAlignment.center,
+                            runSpacing: 10,
+                            children: [
+                              Text(
+                                "Home",
+
+                                style: TextStyle(
+                                  fontSize: 20,
+                                  fontWeight: FontWeight.bold,
+                                ),
+                              ),
+
+                              Icon(Icons.home),
+                            ],
+                          ),
+                        ),
+                      ),
+                    ),
+                  ],
+                ),
               ],
+            ),
+          ),
+          Expanded(
+            child: Container(
+              color: Colors.white,
+              child: const Center(child: Text("Yahan Dashboard ka data ayega")),
             ),
           ),
         ],
