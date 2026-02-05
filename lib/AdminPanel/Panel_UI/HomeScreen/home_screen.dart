24c9cb30de7b7611084911e0927d41d64279fc1f
arshadflutterdev <arshadcodess@gmail.com>
2026-02-05 12:26:29 +0500

    changes

--- lib/AdminPanel/Panel_UI/HomeScreen/home_screen.dart
+++ lib/AdminPanel/Panel_UI/HomeScreen/home_screen.dart
@@ -11,6 +11,33 @@ class HomeScreen extends StatefulWidget {
 class _HomeScreenState extends State<HomeScreen> {
   @override
   Widget build(BuildContext context) {
-    return Scaffold(backgroundColor: AppColours.bg,body: ,);
+    final height = MediaQuery.of(context).size.height;
+    final width = MediaQuery.of(context).size.width;
+
+    return Scaffold(
+      backgroundColor: AppColours.bg,
+      body: Row(
+        children: [
+          Container(
+            width: width * 0.15,
+            decoration: BoxDecoration(color: Colors.blue),
+            child: Column(
+              children: [
+                SizedBox(
+                  height: 60,
+                  width: double.infinity,
+                  child: DrawerHeader(
+                    child: Text(
+                      "Panda Admin",
+                      style: TextStyle(fontSize: 16, color: Colors.white),
+                    ),
+                  ),
+                ),
+              ],
+            ),
+          ),
+        ],
+      ),
+    );
   }
 }
