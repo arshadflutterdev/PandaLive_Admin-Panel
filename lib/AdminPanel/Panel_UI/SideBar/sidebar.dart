b06350be2b13967c3200b25b6fce9ca6ee69b6a5
arshadflutterdev <arshadcodess@gmail.com>
2026-02-09 10:54:52 +0500

    changes

--- lib/AdminPanel/Panel_UI/SideBar/sidebar.dart
+++ lib/AdminPanel/Panel_UI/SideBar/sidebar.dart
@@ -38,98 +38,18 @@ class _SideBarScreenState extends State<SideBarScreen> {
 
     return Scaffold(
       backgroundColor: AppColours.bg,
-      body: Row(
-        children: [
-          Container(
-            width: width * 0.14,
-            decoration: BoxDecoration(color: Color(0xff0D1A63)),
-            child: Column(
-              mainAxisAlignment: MainAxisAlignment.start,
-              children: [
-                Gap(10),
-                Center(
-                  child: Text(
-                    "Panda Admin",
-                    style: TextStyle(fontSize: 22, color: Colors.white),
-                  ),
-                ),
-                Center(
-                  child: Text(
-                    "zahrani@gmail.com",
-                    style: TextStyle(fontSize: 12, color: Colors.white),
-                  ),
-                ),
-                Gap(20),
-                Expanded(
-                  child: ListView.builder(
-                    itemCount: tabNames.length,
-                    itemBuilder: (context, index) {
-                      return Padding(
-                        padding: const EdgeInsets.symmetric(
-                          horizontal: 10,
-                          vertical: 3,
-                        ),
-                        child: GestureDetector(
-                          onTap: () {
-                            selectedIndex.value = index;
-                          },
-                          child: Obx(
-                            () => Container(
-                              width: double.infinity,
-                              decoration: BoxDecoration(
-                                color: selectedIndex.value == index
-                                    ? Colors.black
-                                    : Colors.white,
-                                borderRadius: BorderRadius.circular(10),
-                              ),
-                              child: Padding(
-                                padding: const EdgeInsets.symmetric(
-                                  vertical: 8,
-                                ),
-                                child: Wrap(
-                                  spacing: 10,
-                                  alignment: WrapAlignment.center,
-                                  runSpacing: 10,
-                                  children: [
-                                    Text(
-                                      tabNames[index].toString(),
+      bottomNavigationBar: BottomNavigationBar(
+        backgroundColor: Color(0xff0D1A63),
 
-                                      style: TextStyle(
-                                        fontSize: 18,
-                                        fontWeight: FontWeight.bold,
-                                        color: selectedIndex.value == index
-                                            ? Colors.white
-                                            : Colors.black,
-                                      ),
-                                    ),
-                                    Icon(
-                                      tabIcons[index].icon,
-                                      color: selectedIndex.value == index
-                                          ? Colors.white
-                                          : Colors.black,
-                                    ),
-                                  ],
-                                ),
-                              ),
-                            ),
-                          ),
-                        ),
-                      );
-                    },
-                  ),
-                ),
-              ],
-            ),
-          ),
-
-          Expanded(
-            child: Container(
-              color: Colors.white,
-              child: Obx(() => adminScreens[selectedIndex.value]),
-            ),
-          ),
+        selectedItemColor: Colors.white,
+        items: [
+          BottomNavigationBarItem(icon: Icon(Icons.home), label: "Home"),
+          BottomNavigationBarItem(icon: Icon(Icons.home), label: "Home"),
+          BottomNavigationBarItem(icon: Icon(Icons.home), label: "Home"),
+          BottomNavigationBarItem(icon: Icon(Icons.home), label: "Home"),
         ],
       ),
+      body: adminScreens[selectedIndex.value],
     );
   }
 }
