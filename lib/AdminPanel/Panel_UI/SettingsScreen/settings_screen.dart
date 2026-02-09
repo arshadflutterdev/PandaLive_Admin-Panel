af23e21b4ac755f5d99d52752cd9450e1327b512
arshadflutterdev <arshadcodess@gmail.com>
2026-02-09 12:27:26 +0500

    changes

--- lib/AdminPanel/Panel_UI/SettingsScreen/settings_screen.dart
+++ lib/AdminPanel/Panel_UI/SettingsScreen/settings_screen.dart
@@ -1,5 +1,7 @@
 import 'package:flutter/material.dart';
+import 'package:gap/gap.dart';
 import 'package:panda_adminpanel/AdminPanel/Utils/Constants/app_colours.dart';
+import 'package:panda_adminpanel/AdminPanel/Widgets/Buttons/my_elevatedbutton.dart';
 
 class SettingsScreen extends StatefulWidget {
   const SettingsScreen({super.key});
@@ -12,7 +14,33 @@ class _SettingsScreenState extends State<SettingsScreen> {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      body: Center(child: Text("Settings")),
+      body: Center(
+        child: Column(
+          mainAxisAlignment: MainAxisAlignment.center,
+          crossAxisAlignment: CrossAxisAlignment.center,
+          children: [
+            Text("Choice Your Favourite Language"),
+            Gap(10),
+            SizedBox(
+              width: 120,
+              child: MyElevatedButton(
+                onpressed: () {},
+                bcolor: Colors.white,
+                child: Text("Arabic", style: TextStyle(color: Colors.black)),
+              ),
+            ),
+            Gap(5),
+            SizedBox(
+              width: 120,
+              child: MyElevatedButton(
+                onpressed: () {},
+                bcolor: Colors.white,
+                child: Text("English", style: TextStyle(color: Colors.black)),
+              ),
+            ),
+          ],
+        ),
+      ),
       backgroundColor: AppColours.bg,
     );
   }
