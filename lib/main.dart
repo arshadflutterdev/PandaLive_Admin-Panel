b0362a6a8de670425277f1ad8dd07e280a7286b5
arshadflutterdev <arshadcodess@gmail.com>
2026-02-09 15:10:22 +0500

    changes

--- lib/main.dart
+++ lib/main.dart
@@ -4,21 +4,28 @@ import 'package:panda_adminpanel/AdminPanel/Routes/app_routes.dart';
 import 'package:panda_adminpanel/AdminPanel/Service/app_language.dart';
 import 'package:panda_adminpanel/firebase_options.dart';
 import 'package:get/get.dart';
+import 'package:shared_preferences/shared_preferences.dart';
 
 void main() async {
   WidgetsFlutterBinding.ensureInitialized();
   await Firebase.initializeApp(options: DefaultFirebaseOptions.currentPlatform);
-  runApp(const MyApp());
+  SharedPreferences sharedPreferences = await SharedPreferences.getInstance();
+  final getSavelang = sharedPreferences.getString("language");
+  Locale initialLocale = getSavelang == 'ar'
+      ? const Locale('ar', 'AE')
+      : const Locale('en', 'US');
+  runApp(MyApp(initialLocale: initialLocale));
 }
 
 class MyApp extends StatelessWidget {
-  const MyApp({super.key});
+  final Locale initialLocale;
+  const MyApp({super.key, required this.initialLocale});
 
   @override
   Widget build(BuildContext context) {
     return GetMaterialApp(
       translations: AppLanguage(),
-      locale: Locale("en", "US"),
+      locale: initialLocale,
       fallbackLocale: Locale('ar', 'UE'),
       debugShowCheckedModeBanner: false,
       getPages: AppRoutes.routes,
