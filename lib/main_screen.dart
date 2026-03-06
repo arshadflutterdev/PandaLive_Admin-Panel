9b314372ddba124022927dc62d4e94ca618de59e
arshadflutterdev <arshadcodess@gmail.com>
2026-03-06 21:27:47 +0500

    changes

--- lib/main_screen.dart
+++ lib/main_screen.dart
@@ -1,4 +1,6 @@
 import 'package:flutter/material.dart';
+import 'package:get/get.dart';
+import 'package:panda_adminpanel/AdminPanel/Routes/app_routes.dart';
 import 'package:panda_adminpanel/AdminPanel/Utils/Constants/app_colours.dart';
 
 class MainScreen extends StatefulWidget {
@@ -23,37 +25,39 @@ class _MainScreenState extends State<MainScreen> {
         gridDelegate: SliverGridDelegateWithFixedCrossAxisCount(
           crossAxisCount: 3,
           childAspectRatio: 2,
-          crossAxisSpacing: 10, // Grid gaps set to professional level
-          mainAxisSpacing: 10, // Grid gaps set to professional level
+          crossAxisSpacing: 10,
+          mainAxisSpacing: 10,
           mainAxisExtent: 120,
         ),
         itemBuilder: (context, index) {
-          return Container(
-            decoration: BoxDecoration(
-              color: Colors.white,
-              borderRadius: BorderRadius.circular(
-                10,
-              ), // Set to professional level corner radius
-              boxShadow: [
-                BoxShadow(
-                  color: Colors.black12, // Reduced color to professional level
-                  blurRadius: 4, // Set to professional level smoothness
-                  offset: Offset(
-                    0,
-                    2,
-                  ), // Offset set to professional level shadow direction
-                ),
-              ],
-            ),
-            child: Padding(
-              padding: const EdgeInsets.all(16.0),
-              child: Column(
-                children: [
-                  Text(
-                    "Sample Item ${index + 1}",
-                    style: TextStyle(fontSize: 18),
+          return GestureDetector(
+            onTap: () {
+              if (index == 0) {
+                Get.toNamed(AppRoutes.login);
+              }
+            },
+            child: Container(
+              decoration: BoxDecoration(
+                color: Colors.white,
+                borderRadius: BorderRadius.circular(10),
+                boxShadow: [
+                  BoxShadow(
+                    color: Colors.black12,
+                    blurRadius: 4,
+                    offset: Offset(0, 2),
                   ),
                 ],
+              ),
+              child: Padding(
+                padding: const EdgeInsets.all(16.0),
+                child: Column(
+                  children: [
+                    Text(
+                      "Sample Item ${index + 1}",
+                      style: TextStyle(fontSize: 18),
+                    ),
+                  ],
+                ),
               ),
             ),
           );
