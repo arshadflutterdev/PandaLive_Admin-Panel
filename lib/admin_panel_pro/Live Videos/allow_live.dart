d41e219a0970cf7bff3fafd3b7da4735fbcb51e9
arshadflutterdev <arshadcodess@gmail.com>
2026-03-17 21:17:04 +0500

    changes

---
+++ lib/admin_panel_pro/Live Videos/allow_live.dart
@@ -0,0 +1,15 @@
+import 'package:flutter/material.dart';
+
+class AllowLiveUser extends StatefulWidget {
+  const AllowLiveUser({super.key});
+
+  @override
+  State<AllowLiveUser> createState() => _AllowLiveUserState();
+}
+
+class _AllowLiveUserState extends State<AllowLiveUser> {
+  @override
+  Widget build(BuildContext context) {
+    return const Placeholder();
+  }
+}
